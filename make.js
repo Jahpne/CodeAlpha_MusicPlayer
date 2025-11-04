7566d74f3d67530362201847ee6232be36c0709e
Jahpne <155178306+Jahpne@users.noreply.github.com>
2025-11-04 19:22:01 +0100

    Refactor playlist data with relative paths
    Updated playlist data to use relative paths for audio and cover images.

--- make.js
+++ make.js
@@ -27,41 +27,42 @@
                 title: "Angels In The Sky",
                 artist: "Polo G",
                 album: "HOOD POET",
-                src: "assets/Angels.mp3",
-                cover: "assets/15.jpg",
+                src: "Angels.mp3",
+                cover: "15.jpg",
                 duration: "3:45"
             },
             {
                 title: "Hero",
                 artist: "Alan Walker & Sasha",
                 album: "Hero",
-                src: "assets/Hero.mp3",
-                cover: "assets/18.jpg",
+                src: "Hero.mp3",
+                cover: "18.jpg",
                 duration: "2:51"
             },
             {
                 title: "Before You Leave Me",
                 artist: "Alex Warren",
                 album: "Broken",
-                src: "assets/Before.mp3",
-                cover: "assets/22.jpg",
+                src: "Before.mp3",
+                cover: "22.jpg",
                 duration: "2:56"
             },
             {
                 title: "Carry You Home",
                 artist: "Alex Warren",
                 album: "55 years?",
-                src: "assets/Home.mp3",
-                cover: "assets/20.jpg",
+                src: "Home.mp3",
+                cover: "20.jpg",
                 duration: "3:10"
             },
             {
                 title: "CRG",
                 artist: "Central Cee",
                 album: "Can't Rush Greatness",
-                src: "assets/Central-Cee-CRG.MP3",
-                cover: "assets/19.jpg",
+                src: "Central-Cee-CRG.MP3",
+                cover: "19.jpg",
                 duration: "3:05"
             }
         ];
+
         
