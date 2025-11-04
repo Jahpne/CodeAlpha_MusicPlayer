a5afb02b6748ea3dc924cc7d46e16d814916decd
Jahpne <155178306+Jahpne@users.noreply.github.com>
2025-11-04 21:42:18 +0100

    Refactor DOM element references and playlist data

--- script.js
+++ script.js
@@ -27,40 +27,40 @@
                 title: "Angels In The Sky",
                 artist: "Polo G",
                 album: "HOOD POET",
-                src: "Angels.mp3",
-                cover: "15.jpg",
+                src: "assets/Angels.mp3",
+                cover: "assets/15.jpg",
                 duration: "3:45"
             },
             {
                 title: "Hero",
                 artist: "Alan Walker & Sasha",
                 album: "Hero",
-                src: "Hero.mp3",
-                cover: "18.jpg",
+                src: "assets/Hero.mp3",
+                cover: "assets/18.jpg",
                 duration: "2:51"
             },
             {
                 title: "Before You Leave Me",
                 artist: "Alex Warren",
                 album: "Broken",
-                src: "Before.mp3",
-                cover: "22.jpg",
+                src: "assets/Before.mp3",
+                cover: "assets/22.jpg",
                 duration: "2:56"
             },
             {
                 title: "Carry You Home",
                 artist: "Alex Warren",
                 album: "55 years?",
-                src: "Home.mp3",
-                cover: "20.jpg",
+                src: "assets/Home.mp3",
+                cover: "assets/20.jpg",
                 duration: "3:10"
             },
             {
                 title: "CRG",
                 artist: "Central Cee",
                 album: "Can't Rush Greatness",
-                src: "Central-Cee-CRG.MP3",
-                cover: "19.jpg",
+                src: "assets/Central-Cee-CRG.MP3",
+                cover: "assets/19.jpg",
                 duration: "3:05"
             }
         ];
@@ -443,3 +443,4 @@
         // Initialize the player when page loads
 
         window.addEventListener('load', initPlayer);
+
